f06b9690eb11cb389d8b9d738285671e0084cc5e
yogesh056 <yogeshwaran.ramesh@codingmart.com>
2020-11-24 14:08:36 +0530

    u

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('instagram', 
-                                'postgres', 
-                                'selva123', {
-  host: 'localhost',
+const sequelize = new Sequelize('d21210ihq09dp0', 
+                                'otbikdlvpflkqf', 
+                                's8a23d5470b1089ed801964e2a6316f827abc4a8171e264a6483cbdbb0837a929', {
+  host: 'ec2-3-216-89-250.compute-1.amazonaws.com',
+  port:5432,
   dialect: 'postgres',
   logging: console.log,
   freezeTableName: true,
